27baf2c8316c5351a84637b8606bc02559cfa5e6
Pranjal <pranjal@smalldaytech.com>
2020-07-27 18:49:50 +0530

    Added cleartextTrafficPermitted

--- app/src/main/java/com/threebm/sampleapp/activity/MainActivity.java
+++ app/src/main/java/com/threebm/sampleapp/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.threebm.sampleapp.activity;
 
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -45,6 +46,30 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         loadSunShineDeatils();
+        progressDoalog = new ProgressDialog(MainActivity.this);
+        ((ProgressDialog) progressDoalog).setMessage("Loading your detail horoscope...");
+        progressDoalog.show();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
     private void loadSunShineDeatils()
     {
@@ -66,27 +91,5 @@ public class MainActivity extends AppCompatActivity {
                 progressDoalog.dismiss();
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
